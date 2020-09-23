2ea5a0ac444872e4dcc9e0385127087eb6aec1ac
Daniela Castrillón <69640416+DanielaCastrill0n@users.noreply.github.com>
2020-09-23 16:48:13 -0500

    Update view.py

--- App/view.py
+++ App/view.py
@@ -1,25 +1,3 @@
-"""
- * Copyright 2020, Departamento de sistemas y Computación
- * Universidad de Los Andes
- *
- *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- """
-
 import sys
 import config
 from DISClib.ADT import list as lt
@@ -29,117 +7,142 @@ assert config
 
 from time import process_time 
 
-"""
-La vista se encarga de la interacción con el usuario.
-Presenta el menu de opciones  y  por cada seleccion
-hace la solicitud al controlador para ejecutar la
-operación seleccionada.
-"""
+#Ruta archivos .csv
 
-# ___________________________________________________
-#  Ruta a los archivos
-# ___________________________________________________
+castingfile = 'Movies/MoviesCastingRaw-small.csv'
+detailsfile = 'Movies/SmallMoviesDetailsCleaned.csv'
 
+#Imprimir información de respuesta
 
-castingfile = 'MoviesCastingRaw-smaller.csv'
-detailsfile = 'SmallerMoviesDetailsCleaned.csv'
-#castingfile = 'AllMoviesCastingRaw.csv'
-#detailsfile = 'AllMoviesDetailsCleaned.csv'
+def printProductionCompany(production): #Imprime las peliculas de una productora de cine determinado
+    t1_start = process_time()
+    if production:
+        print('Productora de cine:' + production)
+        print('Promedio: ' + str(production['vote_average']))
+        print('Total de peliculas: ' + str(lt.size(production['movies'])))
+        iterator = it.newIterator(production['movies'])
+        while it.hasNext(iterator):
+            movie = it.next(iterator)
+            print('Titulo: ' + movie['original_title'])
+    else:
+        print('No se encontro la productora de cine en el catalogo')
+    t1_stop = process_time()
+    print ("Tiempo de ejecucion:", t1_start-t1_stop,"segundos")
 
-# ___________________________________________________
-#  Menu principal
-# ___________________________________________________
+def printDirectorData(director): #Imprime las peliculas de un director determinado
+    t1_start = process_time()
+    if director:
+        print('Director:' + director)
+        print('Promedio:' + str(director['vote_average']))
+        print('Total de peliculas: ' + str(lt.size(director['movies'])))
+        iterator = it.newIterator(director['movies'])
+        while it.hasNext(iterator):
+            movie = it.next(iterator)
+            print('Titulo: ' + movie['original_title'])
+    else:
+        print('No se encontro el director en el catalogo')
+    t1_stop = process_time()
+    print ("Tiempo de ejecucion:", t1_start-t1_stop,"segundos")
 
+def printActorData(actor): #Imprime las peliculas de un actor determinado
+    t1_start = process_time()
+    if actor:
+        print('Actor encontrado: ' + actor)
+        print('Promedio: ' + str(actor['vote_average']))
+        print('Total de peliculas: ' + str(lt.size(actor['movies'])))
+        iterator = it.newIterator(actor['movies'])
+        while it.hasNext(iterator):
+            movie = it.next(iterator)
+            print('Titulo: ' + movie['original_title'])
+    else:
+        print('No se encontro el actor en el catalogo')
+    t1_stop = process_time()
+    print ("Tiempo de ejecucion:", t1_start-t1_stop,"segundos")
+
+def printGenreData(genero): #Imprime las peliculas de un genero cinematografico determinado
+    t1_start = process_time()
+    if genero:
+        print('Genero cinematografico: ' + genero)
+        print('Promedio: ' + str(genero['vote_count']))
+        print('Total de peliculas: ' + str(lt.size(genero['movies'])))
+        iterator = it.newIterator(genero['movies'])
+        while it.hasNext(iterator):
+            movie = it.next(iterator)
+            print('Titulo: ' + movie['original_title'])
+    else:
+        print('No se encontro el genero cinematografico en el catalogo')
+    t1_stop = process_time()
+    print ("Tiempo de ejecucion:", t1_start-t1_stop,"segundos")
+
+def printMoviesbyCountry(country): #Imprime las peliculas que han sido producidas en un pais
+    t1_start = process_time()
+    print('Se encontraron: ' + str(lt.size(country)) + ' peliculas')
+    iterator = it.newIterator(country)
+    while it.hasNext(iterator):
+        movie = it.next(iterator)
+        print(movie['original_title'])
+    t1_stop = process_time()
+    print ("Tiempo de ejecucion:", t1_start-t1_stop,"segundos")
+
+#Menu principal
 
 def printMenu():
-    print("Bienvenido")
+    print("***Bienvenido***")
     print("1- Inicializar Catálogo")
-    print("2- Descubrir productoras de cine")
-    print("3- Conocer un director")
-    print("4- Conocer un actor")
-    print("5- Entender un género cinematográfico")
-    print("6- Encontrar películas por país")
+    print("2- Cargar información en el catálogo")
+    print("3- Descubrir productora de cine")
+    print("4- Conocer un director")
+    print("5- Conocer un actor")
+    print("6- Entender un genero cinematografico")
+    print("7- Encontrar peliculas por pais")
     print("0- Salir")
 
+#Ejecutar menu principal
 
-"""
-Menu principal
-"""
 while True:
     printMenu()
-    inputs = input('Seleccione una opción para continuar:\n')
+    inputs = input('Seleccione una opción para continuar\n')
 
     if int(inputs[0]) == 1:
-        print("Inicializando Catálogo...")
-        # cont es el controlador que se usará de acá en adelante
-        t1_start = process_time()
-        contCast = controller.initCatalogCasting()
-        contDet = controller.initCatalogDetails()
-        t1_stop = process_time()
-        print ("Tiempo de ejecución:", t1_stop-t1_start,"segundos\n")
+        print("Inicializando Catálogo...") #cont es el controlador que se usará de acá en adelante
+        cont = controller.initCatalog()
 
     elif int(inputs[0]) == 2:
-        print("Cargando archivos en Catalogos...")
-        controller.loadData(contCast, contDet, castingfile, detailsfile)
-        print('Detalles cargados: ' + str(controller.detSize(contDet)))
-        print('Casting cargados: ' + str(controller.castSize(contCast)))
-
-    elif int(inputs[0]) == 3:
-        company_name = input("Ingrese la productora de cine a buscar:")
+        print("Cargando información de los archivos...")
+        controller.loadData(cont, detailsfile, castingfile)
         t1_start = process_time()
-        production = controller.getMoviesByProductionCompany(contDet, company_name)
-        if production != None:
-            print(production)
-        else:
-            print ("No existe esta productora de cine en el catalogo.")
+        print('Productoras de cine cargadas: ' + str(controller.productionCompanySize(cont)))
+        print('Directores cargadas: ' + str(controller.directorSize(cont)))
+        print('Actores cargados: ' + str(controller.actorSize(cont)))
+        print('Géneros cinematograficos cargados: ' + str(controller.generosSize(cont)))
+        print('Paises cargados: ' + str(controller.countrySize(cont)))
         t1_stop = process_time()
-        print ("Tiempo de ejecución:", t1_stop-t1_start,"segundos\n")
+        print ("Tiempo de ejecucion:", t1_start-t1_stop,"segundos")
 
-    elif int(inputs[0]) == 4:
-        director_name = input("Ingrese el nombre del director a buscar:")
-        t1_start = process_time()
-        director = controller.getMoviesByDirector(contCast, director_name)
-        if director != None:
-            print(director)
-        else:
-            print ("No existe este director en el catalogo.")
-        t1_stop = process_time()
-        print ("Tiempo de ejecución:", t1_stop-t1_start,"segundos\n")
+    elif int(inputs[0]) == 3: #Productora de cine
+        productionName = input("Nombre de la productora de cine a buscar: ")
+        productioninfo = controller.getMoviesByProductionCompany(cont, productionName)
+        printProductionCompany(productioninfo)
 
-    elif int(inputs[0]) == 5:
-        actor_name = input("Ingrese el nombre del actor a buscar:")
-        t1_start = process_time()
-        actor = controller.getMoviesByActor(contCast, actor_name)
-        if actor != None:
-            print(actor)
-        else:
-            print ("No existe este actor en el catalogo.")
-        t1_stop = process_time()
-        print ("Tiempo de ejecución:", t1_stop-t1_start,"segundos\n")
+    elif int(inputs[0]) == 4: #Director
+        directorName = input("Nombre del director a buscar: ")
+        directorinfo = controller.getMoviesByDirector(cont, directorName)
+        PrintDirectorData(directorinfo)
 
-    elif int(inputs[0]) == 6:
-        genre = input("Ingrese el nombre del genero a buscar:")
-        t1_start = process_time()
-        genero = controller.getMoviesByGenre(contDet, genre)
-        if genero != None:
-            print(genero)
-        else:
-            print ("No existe este genero en el catalogo.")
-        t1_stop = process_time()
-        print ("Tiempo de ejecución:", t1_stop-t1_start,"segundos\n")
+    elif int(inputs[0]) == 5: #Actor
+        actorName = input("Nombre del actor a buscar: ")
+        actorinfo = controller.getMoviesByActor(cont, actorName)
+        printActorData(actorinfo)
 
-    elif int(inputs[0]) == 7:
-        conuntry_name = input("Ingrese el nombre del pais a buscar:")
-        t1_start = process_time()
-        pais = controller.getMoviesByCountry(contDet, conuntry_name)
-        if pais != None:
-            print(director)
-        else:
-            print ("No existe este pais en el catalogo.")
-        t1_stop = process_time()
-        print ("Tiempo de ejecución:", t1_stop-t1_start,"segundos\n")
+    elif int(inputs[0]) == 6: #Genero cinematografico
+        genreName = input("Nombre del genero cinematografico a buscar: ")
+        generoinfo = controller.getMoviesByGenre(cont, genreName)
+        printGenreData(generoinfo)
 
+    elif int(inputs[0]) == 7: #Pais
+        coutryName = input("Nombre del pais a buscar: ")
+        countryinfo = controller.getMoviesByCountry(cont, coutryName)
+        printMoviesbyCountry(countryinfo)
     else:
         sys.exit(0)
 sys.exit(0)
-
