b15ae033c1735c22dcb24de5751a088587569fdc
Daniela Castrillón <69640416+DanielaCastrill0n@users.noreply.github.com>
2020-09-23 16:47:35 -0500

    Update controller.py

--- App/controller.py
+++ App/controller.py
@@ -1,131 +1,66 @@
-"""
- * Copyright 2020, Departamento de sistemas y Computación
- * Universidad de Los Andes
- *
- *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- """
-
 import config as cf
 from App import model
 import csv
 
-"""
-El controlador se encarga de mediar entre la vista y el modelo.
-Existen algunas operaciones en las que se necesita invocar
-el modelo varias veces o integrar varias de las respuestas
-del modelo en una sola respuesta.  Esta responsabilidad
-recae sobre el controlador.
-"""
+#Inicializacion del catalogo
 
-# ___________________________________________________
-#  Inicializacion del catalogo
-# ___________________________________________________
-
-def initCatalogDetails():
-    """
-    Llama la funcion de inicializacion del catalogo del modelo.
-    """
-    # catalog es utilizado para interactuar con el modelo
-    catalog = model.newCatalogDetails()
+def initCatalog(): 
+    #Llama la funcion de inicializacion del catalogo del modelo.
+    # catalog es utilizado para interactuar con el modelo.
+    catalog = model.newCatalog()
     return catalog
 
-def initCatalogCasting():
-    """
-    Llama la funcion de inicializacion del catalogo del modelo.
-    """
-    # catalog es utilizado para interactuar con el modelo
-    catalog = model.newCatalogCasting()
-    return catalog
-# _____________________________________________________________________________
-#  Funciones para la carga de datos y almacenamiento de datos en los modelos
-# _____________________________________________________________________________
+#Funciones para la carga de datos y almacenamiento en los modelos
 
-def loadData(catalogoCast, catalogoDet, castingfile, detailsfile):
-    """
-    Carga los datos de los archivos en el modelo
-    """
-    loadDetails(catalogoDet, detailsfile)
-    loadCasting(catalogoCast, castingfile)
-
-
-def loadDetails(catalog, detailsfile):
-    """
-    Carga la información que asocia detalles con peliculas.
-    Primero se localiza el tag y se le agrega la información leida.
-    Adicionalmente se le agrega una referencia al libro procesado.
-    """
-    detailsfile = cf.data_dir + detailsfile
-    with open(detailsfile, encoding="utf-8") as r:
-        key = list(r)[0]
-        for value in r:
-            value = value.split(";")
-            model.addDetails(catalog, key, value)
-            
+def loadData(catalog, castingfile, detailsfile): #Carga los datos al modelo
+    loadCasting(catalog, castingfile)
+    loadDetails(catalog, detailsfile)
 
 def loadCasting(catalog, castingfile):
-    """
-    Carga la información que asocia casting con peliculas.
-    Primero se localiza el tag y se le agrega la información leida.
-    Adicionalmente se le agrega una referencia al libro procesado.
-    """
     castingfile = cf.data_dir + castingfile
-    with open(castingfile, encoding="utf-8") as r:
-        key = list(r)[0]
-        for value in r:
-            value = value.split(";")
-            model.addDetails(catalog, key, value)
+    input_file = csv.DictReader(open(castingfile,'r',encoding='utf-8', errors='ignore'))
+    for tag in input_file:
+        model.addCasting(catalog, tag)
 
-# ___________________________________________________
-#  Funciones para consultas
-# ___________________________________________________
+def loadDetails(catalog, detailsfile):
+    detailsfile = cf.data_dir + detailsfile
+    input_file = csv.DictReader(open(detailsfile,'r',encoding='utf-8', errors='ignore'))
+    for tag in input_file:
+        model.addDetails(catalog, tag)
 
-def detSize(catalog):
-    """
-    Del archivo Details
-    """
-    return model.detSize(catalog)
+#Funciones para consultas
 
-def castSize(catalog):
-    """
-    Del archivo Casting
-    """
-    return model.castSize(catalog)
+def productionCompanySize(catalog):
+    return model.productionCompanySize(catalog)
 
-# ____________________________________
-#  Funciones Requerimientos Reto 2
-# ____________________________________
+def directorSize(catalog):
+    return model.directorSize(catalog)
 
-def getMoviesByProductionCompany(catalog, company_name):
-    productioninfo = model.getmoviesByProductionCompany(catalog, company_name)
+def actorSize(catalog):
+    return model.actorSize(catalog)
+
+def generosSize(catalog):
+    return model.generosSize(catalog)
+
+def countrySize(catalog):
+    return model.countrySize(catalog)
+
+def getMoviesByProductionCompany(catalog, productionName):
+    productioninfo = model.getMoviesByProductionCompany(catalog, productionName)
     return productioninfo
 
-def getMoviesByDirector(catalog, director_name):
-    directorinfo = model.getMoviesByDirector(catalog, director_name)
+def getMoviesByDirector(catalog, directorName):
+    directorinfo = model.getMoviesByDirector(catalog, directorName)
     return directorinfo
 
-def getMoviesByActor(catalog, actor_name):
-    actorinfo = model.getMoviesByActor(catalog, actor_name)
+def getMoviesByActor(catalog, actorName):
+    actorinfo = model.getMoviesByActor(catalog, actorName)
     return actorinfo
 
-def getMoviesByGenre(catalog, genre):
-    genreinfo = model.getMoviesByGenre(catalog, genre)
-    return genreinfo
-    
-def getMoviesByCountry(catalog, country_name):
-    countryinfo = model.getMoviesByCountry(catalog, country_name)
+def getMoviesByGenre(catalog, genreName):
+    generoinfo = model.getMoviesByGenre(catalog, genreName)
+    return generoinfo
+
+def getMoviesByCountry(catalog, countryName):
+    countryinfo = model.getMoviesByCountry(catalog, countryName)
     return countryinfo
