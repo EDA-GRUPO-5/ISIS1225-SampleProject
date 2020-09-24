b5a4646e33ae623d0cce9657fdd277e0b19160e3
Daniela Castrillón <69640416+DanielaCastrill0n@users.noreply.github.com>
2020-09-24 01:16:45 -0500

    Update view.py

--- App/view.py
+++ App/view.py
@@ -12,86 +12,14 @@ from time import process_time
 
 files = ("Data/SmallMoviesDetailsCleaned.csv", "Data/AllMoviesCastingRaw.csv")
 
-
-#Imprimir información de respuesta
-
-def printProductionCompany(production): #Imprime las peliculas de una productora de cine determinado
-    t1_start = process_time()
-    if production:
-        print('Productora de cine:' + production)
-        print('Promedio: ' + str(production['vote_average']))
-        print('Total de peliculas: ' + str(lt.size(production['movies'])))
-        iterator = it.newIterator(production['movies'])
-        while it.hasNext(iterator):
-            movie = it.next(iterator)
-            print('Titulo: ' + movie['original_title'])
-    else:
-        print('No se encontro la productora de cine en el catalogo')
-    t1_stop = process_time()
-    print ("Tiempo de ejecucion:", t1_stop-t1_start,"segundos")
-
-def printDirectorData(director): #Imprime las peliculas de un director determinado
-    t1_start = process_time()
-    if director:
-        print('Director:' + director)
-        print('Promedio:' + str(director['vote_average']))
-        print('Total de peliculas: ' + str(lt.size(director['movies'])))
-        iterator = it.newIterator(director['movies'])
-        while it.hasNext(iterator):
-            movie = it.next(iterator)
-            print('Titulo: ' + movie['original_title'])
-    else:
-        print('No se encontro el director en el catalogo')
-    t1_stop = process_time()
-    print ("Tiempo de ejecucion:", t1_stop-t1_start,"segundos")
-
-def printActorData(actor): #Imprime las peliculas de un actor determinado
-    t1_start = process_time()
-    if actor:
-        print('Actor encontrado: ' + actor)
-        print('Promedio: ' + str(actor['vote_average']))
-        print('Total de peliculas: ' + str(lt.size(actor['movies'])))
-        iterator = it.newIterator(actor['movies'])
-        while it.hasNext(iterator):
-            movie = it.next(iterator)
-            print('Titulo: ' + movie['original_title'])
-    else:
-        print('No se encontro el actor en el catalogo')
-    t1_stop = process_time()
-    print ("Tiempo de ejecucion:", t1_stop-t1_start,"segundos")
-
-def printGenreData(genero): #Imprime las peliculas de un genero cinematografico determinado
-    t1_start = process_time()
-    if genero:
-        print('Genero cinematografico: ' + genero)
-        print('Promedio: ' + str(genero['vote_count']))
-        print('Total de peliculas: ' + str(lt.size(genero['movies'])))
-        iterator = it.newIterator(genero['movies'])
-        while it.hasNext(iterator):
-            movie = it.next(iterator)
-            print('Titulo: ' + movie['original_title'])
-    else:
-        print('No se encontro el genero cinematografico en el catalogo')
-    t1_stop = process_time()
-    print ("Tiempo de ejecucion:", t1_stop-t1_start,"segundos")
-
-def printMoviesbyCountry(country): #Imprime las peliculas que han sido producidas en un pais
-    t1_start = process_time()
-    print('Se encontraron: ' + str(lt.size(country)) + ' peliculas')
-    iterator = it.newIterator(country)
-    while it.hasNext(iterator):
-        movie = it.next(iterator)
-        print(movie['original_title'])
-    t1_stop = process_time()
-    print ("Tiempo de ejecucion:", t1_stop-t1_start,"segundos")
-
 #Menu principal
 
 def printMenu():
     print("\nBienvenido")
     print("1- Cargar Datos")
     print("2- Descubrir productoras de cine")
-    print("4- Enteder un género")
+    print("3- Conocer director
+    print("4- Entender un género")
     print("0- Salir")
 
 #Ejecutar menu principal
@@ -113,6 +41,13 @@ while True:
         t1_start = process_time()
         productora = input("\nIngrese el nombre de la productora: ")
         controller.iniciarDescubrirProductoras(catalogo, productora)
+        t1_stop = process_time()
+        print ("\nTiempo de ejecucion:", t1_stop-t1_start,"segundos")
+          
+    elif int(inputs[0]) == 3:  #opcion 3
+        t1_start = process_time()
+        director = input("\nIngrese el nombre del director: ")
+        controller.iniciarConocerDirector(catalogo, director)
         t1_stop = process_time()
         print ("\nTiempo de ejecucion:", t1_stop-t1_start,"segundos")
     
